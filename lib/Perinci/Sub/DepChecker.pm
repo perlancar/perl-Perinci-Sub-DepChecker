7a6c6239de1e56c7a8f974ba848abb8857287dfe
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-05-02 10:14:24 +0700

    Change 'exec' clause to 'prog' (Rinci 1.1.14) but still support 'exec' for now; Add doc

--- lib/Perinci/Sub/DepChecker.pm
+++ lib/Perinci/Sub/DepChecker.pm
@@ -65,27 +65,34 @@ sub checkdep_code {
     $cval->() ? "" : "code doesn't return true value";
 }
 
-sub checkdep_exec {
+sub checkdep_prog {
     my ($cval) = @_;
 
     if ($cval =~ m!/!) {
-        return "Executable $cval not available" unless (-x $cval);
+        return "Program $cval not executable" unless (-x $cval);
     } else {
         require File::Which;
-        return "$cval not found in PATH" unless File::Which::which($cval);
+        return "Program $cval not found in PATH"
+            unless File::Which::which($cval);
     }
     "";
 }
+
+# for backward-compatibility
+sub checkdep_exec { checkdep_prog(@_) }
 
 1;
 # ABSTRACT: Check dependencies from 'deps' property
 
 =head1 SYNOPSIS
 
- use Perinci::Spec::DepChecker qw(check_deps);
- my $err = check_deps($spec->{deps});
+ use Perinci::Spec::DepChecker qw(check_deps dep_must_be_satisfied);
+
+ my $err = check_deps($meta->{deps});
  print "Dependencies not met: $err" if $err;
 
+ print "Dep foo must be satisfied"
+     if dep_must_be_satisfied('foo', $meta->{deps});
 
 =head1 DESCRIPTION
 
@@ -95,8 +102,40 @@ module performs check on it.
 This module is mainly used by L<Perinci::Sub::Wrapper>.
 
 
+=head1 FUNCTIONS
+
+None is exported by default, but every function is exportable.
+
+=head2 check_deps($deps_clause) => ERRSTR
+
+Check dependencies. Will in turn call various C<checkdep_NAME()> subroutines.
+Return empty string if all dependencies are met, or a string containing an error
+message stating a dependency error.
+
+Example:
+
+ my $err = check_deps({env=>'DEBUG'});
+
+Will check environment variable C<DEBUG>. If true, will return an empty string.
+Otherwise will set $err with something like C<Environment variable DEBUG not
+set/true>.
+
+Another example:
+
+ my $err = check_deps({ all=>[{env=>"A"}, {env=>"B", prog=>"bc"}] });
+
+The above will check environment variables C<A>, C<B>, as well as program C<bc>.
+All dependencies must be met (because we use the C<and> metaclause).
+
+To support a custom dependency named C<NAME>, just define C<checkdep_NAME>
+subroutine in L<Perinci::Sub::DepChecker> package which accepts a value and
+should return an empty string on success or an error message string.
+
+
 =head1 SEE ALSO
 
 L<Perinci>
+
+'deps' section in L<Rinci::function>
 
 =cut
